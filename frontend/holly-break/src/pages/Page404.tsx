26e03f7ea7d1ba66f218d6a570ded0372c34bffe
Lydia <lydia.haway@gmail.com>
2022-09-27 12:06:58 +0200

    page404

--- frontend/holly-break/src/pages/Page404.tsx
+++ frontend/holly-break/src/pages/Page404.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import Footer from "../component/Footer";
 import Header from "../component/Header";
+import img from "../img/404.png";
 
 function Page404() {
   return (
@@ -9,12 +10,17 @@ function Page404() {
       <Header></Header>
       <main className="content-container">
         <div className="container_page_404">
-          <h1>Oops page not found !</h1>
-          <h2>Looks like you're lost !</h2>
-          <p>Don't worry ! This might help you</p>
-          <NavLink to="/Home">
-            <button>Home</button>
-          </NavLink>
+          <div>
+            <h1>Oops page not found !</h1>
+            <h2>Looks like you're lost !</h2>
+            <p>Don't worry ! This might help you</p>
+            <NavLink to="/Home">
+              <button>Home</button>
+            </NavLink>
+          </div>
+          <div>
+            <img src={img} alt="img" />
+          </div>
         </div>
       </main>
       <Footer></Footer>
